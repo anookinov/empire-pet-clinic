bc63383a956ecb809634909b8e284c4752dc6d1e
anookinov <anookinov@gmail.com>
2018-08-26 12:02:20 +0700

    First commit

---
+++ src/main/java/empire/anookinov/empirepetclinic/EmpirePetClinicApplication.java
@@ -0,0 +1,12 @@
+package empire.anookinov.empirepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpirePetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpirePetClinicApplication.class, args);
+	}
+}
