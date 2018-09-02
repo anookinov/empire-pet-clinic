8e59e7b46f0163858f6f8cee6479fb17d1d16148
anookinov <anookinov@gmail.com>
2018-09-02 23:15:38 +0700

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package empire.anookinov.empirepetclinic.services.map;
+
+import empire.anookinov.empirepetclinic.model.Pet;
+import empire.anookinov.empirepetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
