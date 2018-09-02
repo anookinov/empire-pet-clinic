5229879ec1b2d9a8a52c6bc670b2ac3692cb002d
anookinov <anookinov@gmail.com>
2018-09-02 21:23:15 +0700

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package empire.anookinov.empirepetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package empire.anookinov.empirepetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
