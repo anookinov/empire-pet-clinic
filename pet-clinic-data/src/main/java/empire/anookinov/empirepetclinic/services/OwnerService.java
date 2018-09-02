bedc06efd4dc6084fa61ab4c73c34a4d9aecef9e
anookinov <anookinov@gmail.com>
2018-09-02 22:48:22 +0700

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package empire.anookinov.empirepetclinic.services;
 
 import empire.anookinov.empirepetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner saveOwner(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/empire/anookinov/empirepetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package empire.anookinov.empirepetclinic.services;
 
 import empire.anookinov.empirepetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet saveOwner(Pet pet);
-
-    Set<Pet> findAll();
 }
